71de7b1ef598f789c9a5a0c5e9d50c5a7e1fec44
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-16 18:33:05 +0100

    Added sops

--- hosts/eridanus/default.nix
+++ hosts/eridanus/default.nix
@@ -5,6 +5,18 @@
     ./disk-config.nix
   ];
 
+  # === SOPS Configuration ===
+  sops = {
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    age.keyFile = "/var/lib/sops-nix/key.txt";
+
+    secrets = {
+      "users/xeseuses/hashedPassword" = {
+        neededForUsers = true;
+     };
+    };
+  };	
+
   # === Custom Options ===
   asthrossystems = {
     hostInfo = "Beelink EQ12, Intel N100, 16GB RAM, 2TB NVMe";
@@ -38,7 +50,7 @@
   hardware.enableRedistributableFirmware = true;
 
   # === Networking ===
-  networking.hostName = "caelum";  # ← Fixed: hostName (capital N)
+  networking.hostName = "eridanus";  
   networking.networkmanager.enable = true;
 
   # === SSH ===
@@ -51,13 +63,12 @@
   users.users.xeseuses = {
   isNormalUser = true;
   extraGroups = [ "wheel" "networkmanager" ];
-  
+  hashedPasswordFile = config.sops.secrets."users/xeseuses/hashedPassword" .path;
+
+
   openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDiAHl5MuIuTJHR+CciMPIzF1JNNQMwKvi6hzhHfn7tBG+7SmV2+djMh9YosRbaeI6vYoXAq7QPKUUzSbeex4dO2PvCSRHOOrlRMT790Gyg4biG2nMSWDusMkG17zykUTCH29Xi0HD6rk5VzwFJqVyJY/iEIlA02l3BwjHdqemsjwnkSkEjRGLRw1vVVKak9Pii+4GkgCKpI2js4V4C94urbiUqbBABa/lAM0CKWiF2ftLmQbcoSlkEsvF5eRQXKQTbMjcQ7BdSabNveXP+KxqdizRYZEfZSmPI+kUA4nKRFqqLBVg0krKYhOJB2mV+K7ycKEjLxy/gEiS2wRmBq5i9sP5jqjGuk59dRwQr5N9vEvO9hg39Zr0iTvALTUhUqfbViXCJPU4R0PnxSm2yiVhrWfGCrq0fHZ+cBDnu8YKI1vvpFqqUzZaQnSttJ0gyjuJhNKAG8zX4zFfqxYdaN9NmKJCCzfj5NO/FmzSKoOdCMqpTAZlkaYk4zPi6THfewp1rkxOKrOaSS74YCY6VJeN4Cl+/gjFCMpDE3oTujxrQ1sZfjFlkGwbBUb77UZdPEmvWrijPRiTPjpcR7wTzmUNnrKs+oYm5FdbzG7aaI03jEwuefqGOikwiY7WSLTZ1EfDaqp0I5li7I+0CbGNmEU0gNEW5U1G5FItCPnS4fpcrtw=="
-  ];  # ← Close the SSH keys array here
-  
-  # ✅ hashedPassword goes here (outside the array)
-  hashedPassword = "$6$9AVzw0HHpr1A6pvj$1VnPoaoco0patQRrxnfxICO5rUeBbZL35fOFN.4UwF/sVBoAzY91o.Dzlsw6iQm9r3P3Jqz3m3vhGNrZE.ex8/";
+  ];  
 };
 
   # === Security ===
