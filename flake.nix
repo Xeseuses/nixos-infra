2b69f423a96aa415adc76d263510fdb6e44111a0
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-16 23:27:29 +0100

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,81 +1,73 @@
+# flake.nix
 {
   description = "NixOS Infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-  
-    impermanence.url = "github:nix-community/impermanence";
-
+    
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    sops-nix = {
+    
+     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }; 
+  
+    # Impermanence
+    impermanence.url = "github:nix-community/impermanence";
+  };
 
-  outputs = { self, nixpkgs, nixos-hardware, disko, sops-nix, ... }: {
+  outputs = { self, nixpkgs, nixos-hardware, disko, sops-nix, impermanence, ... }: {
     nixosConfigurations = {
+      
       eridanus = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
         modules = [
-     	  # Custom Options
           ./modules/options.nix
-	  
-	  # Common modules
           ./modules/nixos/common
-   
- 	  # Add backup module 
           ./modules/nixos/optional/backup.nix
-
- 	  # Disko
           disko.nixosModules.disko
-
-	  # Sops
           sops-nix.nixosModules.sops
-	 
- 	  # Host config
           ./hosts/eridanus
-	
-	  # Hardware
           nixos-hardware.nixosModules.common-cpu-intel
           nixos-hardware.nixosModules.common-pc-ssd
-
-	vela = nixpkgs.lib.nixosSystem {
+        ];
+      };
+      
+      vela = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./modules/options.nix
           ./modules/nixos/common
           
-     
-   	  # Impermanence
+          # Impermanence
           impermanence.nixosModules.impermanence
-
+          
           # Laptop modules
+          ./modules/nixos/optional/impermanence.nix
           ./modules/nixos/optional/backup.nix
           ./modules/nixos/optional/laptop.nix
-          ./modules/nixos/optional/desktop-kde.nix
+          ./modules/nixos/optional/desktop-niri.nix
+          ./modules/nixos/optional/noctalia.nix
           ./modules/nixos/optional/touchscreen.nix
-          ./modules/nixos/optional/graphics-intel.nix
+          ./modules/nixos/optional/graphics-nvidia-hybrid.nix
           ./modules/nixos/optional/asus-rog.nix
           
           disko.nixosModules.disko
           sops-nix.nixosModules.sops
           ./hosts/vela
           
-          # Hardware support
+          # Hardware
           nixos-hardware.nixosModules.common-cpu-intel
           nixos-hardware.nixosModules.common-pc-laptop
           nixos-hardware.nixosModules.common-pc-laptop-ssd
         ];
       };
+      
     };
   };
 }
-
