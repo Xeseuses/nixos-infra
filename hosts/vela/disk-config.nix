877b04eac162670f9b3a6aa078063aabe4ee86a3
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-17 14:16:36 +0100

    Fixed diskconfig for Vela

--- hosts/vela/disk-config.nix
+++ hosts/vela/disk-config.nix
@@ -1,54 +1,34 @@
-cat flake.nix
-# flake.nix
+# hosts/vela/disk-config.nix
+{ ... }:
 {
-  description = "NixOS Infrastructure";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-     sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Impermanence
-    impermanence.url = "github:nix-community/impermanence";
-  };
-
-  outputs = { self, nixpkgs, nixos-hardware, disko, sops-nix, impermanence, ... }: {
-    nixosConfigurations = {
-
-      eridanus = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./modules/options.nix
-          ./modules/nixos/common
-          ./modules/nixos/optional/backup.nix
-          disko.nixosModules.disko
-          sops-nix.nixosModules.sops
-          ./hosts/eridanus
-          nixos-hardware.nixosModules.common-cpu-intel
-          nixos-hardware.nixosModules.common-pc-ssd
-        ];
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
       };
-
-      vela = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./modules/options.nix
-          disko.nixosModules.disko
-          ./hosts/vela
-          nixos-hardware.nixosModules.common-cpu-intel
-        ];
-      };
-
     };
   };
 }
