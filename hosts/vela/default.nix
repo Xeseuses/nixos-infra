8a78c5ee570eddb02b4f15b87b5a1adb19a83c05
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-17 11:15:15 +0100

    Remove conflicting fileSystems - let disko handle everything

--- hosts/vela/default.nix
+++ hosts/vela/default.nix
@@ -1,4 +1,4 @@
-# hosts/vela/default.nix - MINIMAL VERSION
+# hosts/vela/default.nix - CORRECT MINIMAL VERSION
 { config, pkgs, ... }:
 {
   imports = [ ./disk-config.nix ];
@@ -10,7 +10,7 @@
     initrd = {
       availableKernelModules = [
         "xhci_pci"
-        "thunderbolt" 
+        "thunderbolt"
         "nvme"
         "usb_storage"
         "sd_mod"
@@ -26,48 +26,13 @@
     kernelModules = [ "kvm-intel" ];
   };
 
-  # Minimal filesystems (let disko handle the rest)
-  fileSystems."/" = {
-    device = "/dev/mapper/cryptroot";
-    fsType = "btrfs";
-    options = [ "subvol=@" "compress=zstd" "noatime" ];
-  };
-  
-  fileSystems."/boot" = {
-    device = "/dev/nvme0n1p1";
-    fsType = "vfat";
-  };
-  
-  fileSystems."/persist" = {
-    device = "/dev/mapper/cryptroot";
-    fsType = "btrfs";
-    options = [ "subvol=@persist" "compress=zstd" "noatime" ];
-    neededForBoot = true;
-  };
-  
-  fileSystems."/nix" = {
-    device = "/dev/mapper/cryptroot";
-    fsType = "btrfs";
-    options = [ "subvol=@nix" "compress=zstd" "noatime" ];
-  };
-  
-  fileSystems."/var/log" = {
-    device = "/dev/mapper/cryptroot";
-    fsType = "btrfs";
-    options = [ "subvol=@log" "compress=zstd" "noatime" ];
-    neededForBoot = true;
-  };
-  
-  swapDevices = [{
-    device = "/.swapvol/swapfile";
-  }];
+  # NO fileSystems here - disko handles it!
+  # NO swapDevices here - disko handles it!
 
   networking.hostName = "vela";
   networking.networkmanager.enable = true;
   
   time.timeZone = "Europe/Amsterdam";
-  
-  users.users.root.initialPassword = "nixos";
   
   users.users.xeseuses = {
     isNormalUser = true;
@@ -77,12 +42,9 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDiAHl5MuIuTJHR+CciMPIzF1JNNQMwKvi6hzhHfn7tBG+7SmV2+djMh9YosRbaeI6vYoXAq7QPKUUzSbeex4dO2PvCSRHOOrlRMT790Gyg4biG2nMSWDusMkG17zykUTCH29Xi0HD6rk5VzwFJqVyJY/iEIlA02l3BwjHdqemsjwnkSkEjRGLRw1vVVKak9Pii+4GkgCKpI2js4V4C94urbiUqbBABa/lAM0CKWiF2ftLmQbcoSlkEsvF5eRQXKQTbMjcQ7BdSabNveXP+KxqdizRYZEfZSmPI+kUA4nKRFqqLBVg0krKYhOJB2mV+K7ycKEjLxy/gEiS2wRmBq5i9sP5jqjGuk59dRwQr5N9vEvO9hg39Zr0iTvALTUhUqfbViXCJPU4R0PnxSm2yiVhrWfGCrq0fHZ+cBDnu8YKI1vvpFqqUzZaQnSttJ0gyjuJhNKAG8zX4zFfqxYdaN9NmKJCCzfj5NO/FmzSKoOdCMqpTAZlkaYk4zPi6THfewp1rkxOKrOaSS74YCY6VJeN4Cl+/gjFCMpDE3oTujxrQ1sZfjFlkGwbBUb77UZdPEmvWrijPRiTPjpcR7wTzmUNnrKs+oYm5FdbzG7aaI03jEwuefqGOikwiY7WSLTZ1EfDaqp0I5li7I+0CbGNmEU0gNEW5U1G5FItCPnS4fpcrtw=="
     ];
   };
-  
+
   security.sudo.wheelNeedsPassword = false;
-  
   services.openssh.enable = true;
-  
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
   system.stateVersion = "24.11";
 }
