7a8e2a711b9bc95120fe6ac7ec6bb05a8508db4d
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-18 15:21:31 +0100

    Fixed syntax errors

--- hosts/eridanus/default.nix
+++ hosts/eridanus/default.nix
@@ -1,3 +1,4 @@
+# hosts/eridanus/default.nix
 { config, lib, pkgs, ... }:
 {
   imports = [
@@ -8,8 +9,8 @@
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
     age.keyFile = "/var/lib/sops-nix/key.txt";
-
     secrets = {
+      "restic_password" = {};
       "users/xeseuses/hashedPassword" = {
         neededForUsers = true;
       };
@@ -25,16 +26,16 @@
       impermanence = false;
       secureBoot = false;
       encryption = false;
-   
-     binaryCache = {
-      enable = true;
-      server = "cache.home.arpa";
-     };
-   
-      # Enable backups
+
+      # Binary cache server
+      binaryCache = {
+        enable = true;
+        server = "cache.home.arpa";
+      };
+
+      # Backups
       backup = {
         enable = true;
-
         targets = {
           # Backup system config
           system = {
@@ -43,7 +44,7 @@
               "/home/xeseuses/nixos-infra"
               "/etc/nixos"
             ];
-            schedule = "daily";
+            timerConfig = { OnCalendar = "daily"; };
           };
 
           # Backup user data
@@ -53,7 +54,7 @@
               "/home/xeseuses/Documents"
               "/home/xeseuses/.ssh"
             ];
-            schedule = "daily";
+            timerConfig = { OnCalendar = "daily"; };
           };
         };
       };
@@ -79,11 +80,16 @@
   # === Networking ===
   networking.hostName = "eridanus";
 
-  # === User Password from SOPS ===
-  users.users.xeseuses.hashedPasswordFile =
-    config.sops.secrets."users/xeseuses/hashedPassword".path;
+  # === User Configuration ===
+  users.users.xeseuses = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPasswordFile = config.sops.secrets."users/xeseuses/hashedPassword".path;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDiAHl5MuIuTJHR+CciMPIzF1JNNQMwKvi6hzhHfn7tBG+7SmV2+djMh9YosRbaeI6vYoXAq7QPKUUzSbeex4dO2PvCSRHOOrlRMT790Gyg4biG2nMSWDusMkG17zykUTCH29Xi0HD6rk5VzwFJqVyJY/iEIlA02l3BwjHdqemsjwnkSkEjRGLRw1vVVKak9Pii+4GkgCKpI2js4V4C94urbiUqbBABa/lAM0CKWiF2ftLmQbcoSlkEsvF5eRQXKQTbMjcQ7BdSabNveXP+KxqdizRYZEfZSmPI+kUA4nKRFqqLBVg0krKYhOJB2mV+K7ycKEjLxy/gEiS2wRmBq5i9sP5jqjGuk59dRwQr5N9vEvO9hg39Zr0iTvALTUhUqfbViXCJPU4R0PnxSm2yiVhrWfGCrq0fHZ+cBDnu8YKI1vvpFqqUzZaQnSttJ0gyjuJhNKAG8zX4zFfqxYdaN9NmKJCCzfj5NO/FmzSKoOdCMqpTAZlkaYk4zPi6THfewp1rkxOKrOaSS74YCY6VJeN4Cl+/gjFCMpDE3oTujxrQ1sZfjFlkGwbBUb77UZdPEmvWrijPRiTPjpcR7wTzmUNnrKs+oYm5FdbzG7aaI03jEwuefqGOikwiY7WSLTZ1EfDaqp0I5li7I+0CbGNmEU0gNEW5U1G5FItCPnS4fpcrtw=="
+    ];
+  };
 
   # === State Version ===
   system.stateVersion = "24.11";
 }
-
