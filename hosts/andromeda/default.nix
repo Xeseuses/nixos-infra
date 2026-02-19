15de9333bca6f4f7fc71dd6471e345efba7e09af
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-19 12:02:12 +0100

    fixed andromeda networking in default.nix

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -38,23 +38,8 @@
   # ============================================
   # Networking (host)
   # ============================================
-
-  networking = {
-  hostName = "andromeda";
-  useDHCP = false;
-  interfaces.ens1 = {
-    useDHCP = false;
-    ipv4.addresses = [{
-      address = "10.30.30.124";
-      prefixLength = 24;
-    }];
-  };
-  defaultGateway = {
-    address = "10.30.30.1";
-    interface = "ens1";
-  };
-  nameservers = [ "1.1.1.1" "8.8.8.8" ];
-};
+   
+    networking.hostName = "andromeda";
 
   # ============================================
   # Users
