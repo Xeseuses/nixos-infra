037195c1f71946bb2d3639328820e1a2c750d487
Xeseuses <github.com.corrosive843@passmail.net>
2026-02-19 23:39:10 +0100

    Fix VPS public key in andromeda config

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -55,7 +55,7 @@
   peers = [
     {
       # lyra (VPS)
-      publicKey = "jXe+WcEMwBkk57aFNzVasv21GAj5058JqFgIcG0IJxs=";  # We'll generate this
+      publicKey = "1wUDy/NFm7QSCnSGSHd26YDLcN3SUGIy7PePpG/WyU0=";
       allowedIPs = [ "10.200.0.0/24" ];  # Route all traffic through VPS (optional)
       endpoint = "172.245.52.108:51821";
       persistentKeepalive = 25;
