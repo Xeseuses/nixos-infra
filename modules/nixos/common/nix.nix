94a3b0823d4e703b54250025516653acfd863710
Xeseuses <github.com.corrosive843@passmail.net>
2026-03-12 22:20:35 +0100

    Changed cacheing url

--- modules/nixos/common/nix.nix
+++ modules/nixos/common/nix.nix
@@ -1,45 +1,37 @@
+
 { config, lib, pkgs, ...}:
 {
   nix = {
-  
-  # Enable flakes and nix-command 
-  settings = {
-     experimental-features = ["nix-command" "flakes"];
-     
-     # Optimize storage
-     auto-optimise-store = true;
-     
-     # Trusted users (can use nix without sudo for some commands)
-     trusted-users = [ "root" "@wheel" ];
-    
-     # Substituters (binart cache servers)
-     substituters = [
-       "https://cache.nixos.org"
-       "http://10.40.40.117:5000"
-     ];
-    
-    trusted-public-keys = [
+    # Enable flakes and nix-command
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      # Optimize storage
+      auto-optimise-store = true;
+      # Trusted users (can use nix without sudo for some commands)
+      trusted-users = [ "root" "@wheel" ];
+      # Substituters (binary cache servers)
+      substituters = [
+        "https://cache.nixos.org"
+        "http://cache.lan:5000"
+      ];
+      trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-   	"cache.home.arpa:dcmEsNlnkWaKUVNpU+s0sDC6W7+2JvaVKGu8fSRnbNE="
+        "cache.lan:nV+mP0rba5Q3gf/LSxe2AzJgybUYBvFFByWzmcwmG1k="
       ];
     };
-    
     # Automatic garbage collection
     gc = {
-     automatic = true;
-     dates = "weekly";
-     options = "--delete-older-than 30d";
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
     };
-
     # Keep build-time dependencies for easier debugging
     extraOptions = ''
       keep-outputs = true
       keep-derivations = true
     '';
   };
-    
-    # Allow unfree packages (for things like Discord, Steam, etc.)
+  # Allow unfree packages (for things like Discord, Steam, etc.)
   nixpkgs.config.allowUnfree = true;
 }
-      
-    
+
