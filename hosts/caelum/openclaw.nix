868efc6e63b773dd2b0d962bcade04f9ed4238a8
Xeseuses <github.com.corrosive843@passmail.net>
2026-03-12 21:49:10 +0100

    Definitve openclaw setup

--- hosts/caelum/openclaw.nix
+++ hosts/caelum/openclaw.nix
@@ -16,8 +16,9 @@
           --name openclaw \
           -e ANTHROPIC_API_KEY="$ANTHROPIC_API_KEY" \
           -e OLLAMA_HOST=http://10.40.40.106:11434 \
+          -e NODE_PATH="/home/node/.openclaw/node_modules" \
           -v /var/lib/openclaw:/home/node/.openclaw \
-          -p 3000:3000 \
+          -p 3000:18789 \
           ghcr.io/openclaw/openclaw:latest
       ''}";
       ExecStop       = "${pkgs.docker}/bin/docker stop openclaw";
@@ -25,6 +26,7 @@
   };
 
   systemd.tmpfiles.rules = [
-    "d /var/lib/openclaw 0750 root docker -"
+    "d /var/lib/openclaw 0750 1000 1000 -"
+    "d /var/lib/openclaw-matrix-shadow 0750 1000 1000 -"
   ];
 }
