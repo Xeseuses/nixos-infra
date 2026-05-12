ecd687e1ba77d729eb6a8c908abe44b8c6c53ef3
Xeseuses <github.com.corrosive843@passmail.net>
2026-05-12 22:22:14 +0200

    updated crowdsec.nix

---
+++ hosts/lyra/crowdsec.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, lib, ... }:
+
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/crowdsec 0755 crowdsec crowdsec - -"
+    "f /var/lib/crowdsec/online_api_credentials.yaml 0750 crowdsec crowdsec - -"
+    "d /var/log/caddy 0750 caddy caddy -"
+  ];
+
+  services.crowdsec = {
+    enable = true;
+    settings = {
+      general.api.server.enable = true;
+      lapi.credentialsFile = "/var/lib/crowdsec/local_api_credentials.yaml";
+      capi.credentialsFile = "/var/lib/crowdsec/online_api_credentials.yaml";
+    };
+    localConfig = {
+      acquisitions = [
+        {
+          source = "journalctl";
+          journalctl_filter = [ "_SYSTEMD_UNIT=sshd.service" ];
+          labels.type = "syslog";
+        }
+        {
+          filenames = [ "/var/log/caddy/access.log" ];
+          labels.type = "caddy";
+        }
+      ];
+    };
+  };
+
+  services.crowdsec-firewall-bouncer = {
+    enable = true;
+    settings = {
+      mode = "nftables";
+      nftables = {
+        ipv4 = {
+          enabled = true;
+          set-only = false;
+          table = "crowdsec";
+          chain = "crowdsec-chain";
+        };
+        ipv6 = {
+          enabled = true;
+          set-only = false;
+          table = "crowdsec6";
+          chain = "crowdsec6-chain";
+        };
+      };
+    };
+  };
+}
